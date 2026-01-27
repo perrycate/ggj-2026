244bfbab6d21a061a30d77b5a8c105fba2175af2
Perry Cate <thescratcherman@gmail.com>
2026-01-27 12:40:51 -0800

    sync game state independently, separately from individual scenes and such

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -17,10 +17,3 @@ func _physics_process(_delta: float) -> void:
 	velocity = input_direction * speed
 
 	move_and_slide()
-
-	if velocity != Vector2.ZERO:
-		_on_player_moved.rpc(position)
-
-@rpc
-func _on_player_moved(new_position: Vector2):
-	position = new_position
