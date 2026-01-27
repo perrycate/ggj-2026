47ef71b7183e0f2512b9243ac2b8b208245609e8
Perry Cate <thescratcherman@gmail.com>
2026-01-26 16:30:52 -0800

    basic movement-sharing

--- scripts/world/game.gd
+++ scripts/world/game.gd
@@ -3,11 +3,11 @@ extends Node2D
 # TODO add mechanism for getting/sharing this.
 const server_address = "127.0.0.1"
 const PORT = 5000
+const MAX_PEERS = 1
+
+@export var is_watcher = false
 
 func _ready():
-	multiplayer.peer_connected.connect(_on_watcher_connected)
-
-func _on_watcher_connected():
 	pass
 
 func _on_join_button_pressed():
@@ -18,3 +18,14 @@ func _on_join_button_pressed():
 
 	multiplayer.multiplayer_peer = peer
 
+	print("joined")
+
+func _on_host_button_pressed():
+	var peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(PORT, MAX_PEERS)
+	if error:
+		return error
+
+	multiplayer.multiplayer_peer = peer
+
+	print("hosting")
