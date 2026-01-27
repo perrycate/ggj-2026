f6ace3e65bacc7e171d5f6cc1ed10592176c42d0
Perry Cate <thescratcherman@gmail.com>
2026-01-26 17:20:16 -0800

    hide player until button clicked

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 
+# Whether the human playing is actively controlling or merely watching.
+# PC: This feels wrong? Need to think about state management asap.
+var is_active: bool = false
+
 var speed : float = 300
 
+func _ready():
+	pass
+
 func _physics_process(_delta: float) -> void:
+	if !is_active:
+		return
+
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
 
