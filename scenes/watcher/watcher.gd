febbe0d93e65dc7ee3e4535875498ce565840c53
ChrisTaks <christaks35@gmail.com>
2026-01-30 23:26:09 -0800

    camera now follows player and watcher correctly (#17)

--- scenes/watcher/watcher.gd
+++ scenes/watcher/watcher.gd
@@ -3,8 +3,14 @@ extends Node2D
 var camera_list: Array[Camera] = []
 var current_camera_idx: int = 0
 
+@onready var camera_node: Camera2D = $Camera2D
+
 func _enter_tree():
 	pass
+
+func _ready() -> void:
+	if is_multiplayer_authority():
+		camera_node.enabled = true
 
 func get_current_camera() -> Camera:
 	return camera_list[current_camera_idx]
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@onready var camera_node: Camera2D = $Camera2D
+
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 	print("player authority: ", name)
@@ -7,4 +9,6 @@ func _enter_tree():
 	print(global_position)
 
 func _ready():
+	if is_multiplayer_authority():
+		camera_node.enabled = true
 	pass
