68d21559296a90c02a69b33cb2bd477062c80cc5
ChrisTaks <christaks35@gmail.com>
2026-01-31 17:58:43 -0800

    initial drone code added (doesn't work on multiplayer)
    * added .ignore file for my nvim config (stupid uid files)
    
    * added F key for watcher drone deployment
    
    * removed unnecessary lines from the detection functions
    
    * removed unecessary pass lines from functions
    
    * drone layer named
    
    * drone scene created
    
    * initial drone spawning logic added
    
    * created drone script file
    
    * added player to a group to reference it easily in drone script
    
    * only 1 drone can be active at a time (this can change later)

--- scripts/camera/camera.gd
+++ scripts/camera/camera.gd
@@ -4,6 +4,10 @@ class_name Camera
 var change_cooldown: float = 0
 var is_active: bool = false
 var player_detected: bool = false
+var detected_player: CharacterBody2D = null
+
+var drone: PackedScene = preload("res://scenes/drone/drone.tscn")
+var deployed_drone = null
 
 @onready var state_machine = $StateMachine
 
@@ -13,7 +17,8 @@ var player_detected: bool = false
 @onready var acoustic_spotlight: Sprite2D = $Spotlights/AcousticSpotlight
 
 const CHANGE_COOLDOWN_MAX: float = 3.0
-const CAMERA_SPEED: float = 200
+const CAMERA_SPEED: float = 200.0
+const DRONE_SPAWN_DISTANCE: float = 150.0
 
 
 func _enter_tree():
@@ -32,6 +37,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	change_cooldown = clampf(change_cooldown - delta, 0.0, CHANGE_COOLDOWN_MAX)
 
+	if Input.is_action_just_pressed("drone_deploy") and not deployed_drone and player_detected:
+			print("deploying drones!")
+			deploy_drones()
+
 func _physics_process(_delta: float) -> void:
 	if is_active:
 		var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -41,6 +50,16 @@ func _physics_process(_delta: float) -> void:
 
 	move_and_slide()
 
+func deploy_drones():
+	var player_angle = global_position.angle_to_point(detected_player.global_position)
+	var drone_angle = deg_to_rad(180) + player_angle
+	var offset = Vector2(cos(drone_angle), sin(drone_angle)) * DRONE_SPAWN_DISTANCE
+	var final_position = position + offset
+
+	deployed_drone = drone.instantiate()
+	get_tree().current_scene.add_child(deployed_drone)
+	deployed_drone.position = final_position
+
 func change_mask(new_mask) -> void:
 	if change_cooldown != 0:
 		return
@@ -48,22 +67,12 @@ func change_mask(new_mask) -> void:
 	if state_machine.transition_mask(new_mask):
 		change_cooldown = CHANGE_COOLDOWN_MAX
 
-func _on_player_detector_area_exited(_area: Area2D) -> void:
-	print("AREA IS NO LONGER DETECTED!")
-	player_detected = true
-
-func _on_player_detector_area_entered(_area: Area2D) -> void:
-	print("AREA HAS BEEN DETECTED!")
-	player_detected = false
-
-
 func _on_player_not_detected(_body: Node2D) -> void:
-	print("PLAYER IS NO LONGER DETECTED!")
-	player_detected = true
-	pass # Replace with function body.
+	#print("PLAYER IS NO LONGER DETECTED!")
+	player_detected = false
+	detected_player = null
 
 func _on_player_detected(_body: Node2D) -> void:
-	print("PLAYER HAS BEEN DETECTED!")
-	player_detected = false
-	pass # Replace with function body.
-
+	#print("PLAYER HAS BEEN DETECTED!")
+	player_detected = true
+	detected_player = _body
